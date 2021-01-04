c4b849469d31d5f8153adbe2e2472e59f8b37ecf
peihaitao <peihaitao@duocaitou.com>
2021-01-04 14:05:33 +0800

    11周作业

--- Week_10/rpc01/rpcfx-demo-provider/src/main/java/io/kimmking/rpcfx/demo/provider/aop/ZookeeperAspect.java
+++ Week_10/rpc01/rpcfx-demo-provider/src/main/java/io/kimmking/rpcfx/demo/provider/aop/ZookeeperAspect.java
@@ -9,6 +9,7 @@ import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.util.Arrays;
@@ -32,11 +34,11 @@ public class ZookeeperAspect {
     @Autowired
     private ZookeeperProperties properties;
 
-    @Pointcut("execution(* io.kimmking.rpcfx.demo.provider..*.*Service(..))")
-    public void aspect(){}
+//    @Pointcut("execution(* io.kimmking.rpcfx.demo.provider..*.*Service(..))")
+//    public void aspect(){}
 
-    @Before("aspect()")
-    private void before(JoinPoint joinPoint) {
+    @After("execution(* io.kimmking.rpcfx.demo.provider..*.*Service(..)) && @annotation(zookeeperRegister)")
+    private void before(JoinPoint joinPoint, ZookeeperRegister zookeeperRegister) {
         Object target = joinPoint.getTarget();
         String method = joinPoint.getSignature().getName();
         Class<?> clazz = target.getClass();
--- Week_11/README.md
+++ Week_11/README.md
@@ -1,1 +1,12 @@
-学习笔记
+4.（必做）基于 Redis 封装分布式数据操作：
+在 Java 中实现一个简单的分布式锁；
+在com.javaAdvance.redis.lock.core中，使用redisTemplate实现的。
+
+在 Java 中实现一个分布式计数器，模拟减库存。
+在目录com.javaAdvance.redis.lock.inventory中，实现了初始化库存和减库存.
+
+5.（必做）基于 Redis 的 PubSub 实现订单异步处理
+在目录com.javaAdvance.redis.lock.listener中。
+OrderPublish 是消息发送者， RedisListener是消息消费者，监听着一个channel。
+监听的配置信息在RedisPubSubConfig配置中。
+ResultBean是模拟的消息体的结构.
